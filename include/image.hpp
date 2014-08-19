60057610a0c3e10cdd2375c7aed1459d96119459
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-08-19 21:42:26 +0900

    shared_ptrを使用しないように修正し、cloneメソッドの追加

--- include/image.hpp
+++ include/image.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <utility>
 #include <opencv2/core/core.hpp>
 #include <iostream>
 #include <cstdint>
@@ -42,10 +43,10 @@ struct Pixel
 
 
 // 
-struct ProblemImpl
+struct Image
 {
   public:
-    ProblemImpl(cv::Mat img) : _img(img) {}
+    Image(cv::Mat img) : _img(img) {}
 
     std::size_t height() const { return _img.rows; }
     std::size_t width() const { return _img.cols; }
@@ -53,6 +54,11 @@ struct ProblemImpl
 
     cv::Mat & cvMat() { return _img; }
     cv::Mat const & cvMat() const { return _img; }
+
+    Image clone() const
+    {
+        return Image(_img.clone());
+    }
 
   private:
     cv::Mat _img;
@@ -66,7 +72,7 @@ struct ProblemImpl
 class ElementImage
 {
   public:
-    ElementImage(std::shared_ptr<ProblemImpl> const & m, std::size_t r, std::size_t c, std::size_t div_x, std::size_t div_y)
+    ElementImage(Image m, std::size_t r, std::size_t c, std::size_t div_x, std::size_t div_y)
     : _master(m), _pos_x(c), _pos_y(r), _div_x(div_x), _div_y(div_y)
     {}
 
@@ -74,26 +80,54 @@ class ElementImage
     /// 分割画像の高さを返します
     std::size_t height() const
     {
-        return _master->height() / _div_y;
+        return _master.height() / _div_y;
     }
 
 
     /// 分割画像の幅を返します
     std::size_t width() const
     {
-        return _master->width() / _div_x;
+        return _master.width() / _div_x;
     }
 
 
     /// 分割画像の(i, j)に位置するピクセル値を返します
-    Pixel get_pixel(std::size_t y, std::size_t x) const
+    Pixel get_pixel(std::size_t r, std::size_t c) const
     {
-        return _master->get_pixel(y + _pos_y * height(), x + _pos_x * width());
+        return _master.get_pixel(r + _pos_y * height(), c + _pos_x * width());
+    }
+
+
+    /** 元画像に対するスライスで返します。
+    */
+    cv::Mat cvMat()
+    {
+        // sliceなので画像のコピーは発生しない
+        return _master.cvMat()(cv::Rect(_pos_x * width(),
+                                       _pos_y * height(),
+                                       width(),
+                                       height()));
+    }
+
+
+    const cv::Mat cvMat() const
+    {
+        return _master.cvMat()(cv::Rect(_pos_x * width(),
+                                       _pos_y * height(),
+                                       width(),
+                                       height()));
+    }
+
+
+    ElementImage clone() const
+    {
+        auto dst = _master.clone();
+        return ElementImage(dst, _pos_y, _pos_x, _div_x, _div_y);
     }
 
 
   private:
-    const std::shared_ptr<ProblemImpl> _master;
+    Image _master;
     std::size_t _pos_x; // (N, M)に画像が分割されていたとき、(i, j)位置の画像を示すなら i
     std::size_t _pos_y; // (N, M)に画像が分割されていたとき、(i, j)位置の画像を示すなら j]
     std::size_t _div_x;
@@ -106,6 +140,11 @@ class ElementImage
 class Problem
 {
   public:
+    Problem(Image m, std::size_t div_x, std::size_t div_y, int change_cost, int select_cost, std::size_t max_select_times)
+    : _master(m), _div_x(div_x), _div_y(div_y), _change_cost(change_cost), _select_cost(select_cost), _max_select_times(max_select_times)
+    {}
+
+
     /** ローカルに保存してあるppmファイルを読み込みます。
     * arguments:
         * ppm_file_path     = ローカルに保存したppmファイルへのパス
@@ -124,8 +163,7 @@ class Problem
             return null_opt();
         }
 
-        Problem pb;
-        pb._master = std::make_shared<ProblemImpl>(img);
+        Image image(img);
 
         std::ifstream file(ppm_file_path);
         if(file.fail()){
@@ -138,34 +176,37 @@ class Problem
         
         char c;
 
+        std::size_t div_x, div_y;
         getline(file, line);    // 分割数
         std::stringstream ss1(line);
-        ss1 >> c >> pb._div_x >> pb._div_y;
+        ss1 >> c >> div_x >> div_y;
 
+        std::size_t max_select_times;
         getline(file, line);    // 最大選択可能回数
         std::stringstream ss2(line);
-        // ss1 = std::stringstream(line);
-        ss2 >> c >> pb._max_select_times;
+        ss2 >> c >> max_select_times;
 
+        int select_cost, change_cost;
         getline(file, line);    // コスト変換レート
         std::stringstream ss3(line);
         // ss1 = std::stringstream(line);
-        ss3 >> c >> pb._select_cost >> pb._change_cost;
+        ss3 >> c >> select_cost >> change_cost;
 
-        return boost::optional<Problem>(pb);
+        Problem dst(image, div_x, div_y, change_cost, select_cost, max_select_times);
+        return boost::optional<Problem>(std::move(dst));
     }
 
 
     /// 問題画像の高さを返します
-    std::size_t height() const { return _master->height(); }
+    std::size_t height() const { return _master.height(); }
 
 
     /// 問題画像の幅を返します
-    std::size_t width() const { return _master->width(); }
+    std::size_t width() const { return _master.width(); }
 
 
     /// (w, h) = (i, j) ピクセル目のピクセル値を返します
-    Pixel get_pixel(std::size_t y, std::size_t x) const { return _master->get_pixel(y, x); }
+    Pixel get_pixel(std::size_t y, std::size_t x) const { return _master.get_pixel(y, x); }
 
 
     /// 横方向の画像分割数を返します
@@ -177,9 +218,16 @@ class Problem
 
 
     /// インデックス配列におけるi行j列の断片を表すオブジェクトを返します
-    ElementImage get_element(std::size_t r, std::size_t c) const
+    ElementImage get_element(std::size_t r, std::size_t c)
     {
         ElementImage dst(_master, r, c, _div_x, _div_y);
+        return dst;
+    }
+
+
+    const ElementImage get_element(std::size_t r, std::size_t c) const
+    {
+        const ElementImage dst(_master, r, c, _div_x, _div_y);
         return dst;
     }
 
@@ -195,12 +243,19 @@ class Problem
     /// 最大選択可能回数
     std::size_t max_select_times() const { return _max_select_times; }
 
-    cv::Mat & cvMat() { return _master->cvMat(); }
-    cv::Mat const & cvMat() const { return _master->cvMat(); }
+    cv::Mat & cvMat() { return _master.cvMat(); }
+    cv::Mat const & cvMat() const { return _master.cvMat(); }
+
+
+    Problem clone() const
+    {
+        auto dst = _master.clone();
+        return Problem(dst, _div_x, _div_y, _change_cost, _select_cost, _max_select_times);
+    }
 
 
   private:
-    std::shared_ptr<ProblemImpl> _master;
+    Image _master;
     std::size_t _div_x;
     std::size_t _div_y;
     int _change_cost;
