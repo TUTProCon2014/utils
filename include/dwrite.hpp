db5ce8abf40456959a687540a4ffe5176c9b5b65
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-08-22 13:22:20 +0900

    D言語のwirte系関数とほとんど等価な関数の追加

---
+++ include/dwrite.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+
+/**
+http://stackoverflow.com/questions/17671772/c11-variadic-printf-performance
+
+writef(std::cout, "foo % bar % hoge %\n", 1, 2, 3);  みたいに使う
+末尾に改行つかない
+*/
+template <typename Stream>
+void writef(Stream & stream, const char *s)
+{
+    while (*s) {
+        if (*s == '%') {
+            if (*(s + 1) == '%')
+                ++s;
+            else
+                throw "invalid format string: missing arguments";
+        }
+        stream << *s++;
+    }
+}
+
+
+template <typename Stream, typename T, typename... Args>
+void writef(Stream & stream, const char *s, T&& value, Args&&... args)
+{
+    while (*s) {
+        if (*s == '%') {
+            if (*(s + 1) == '%')
+                ++s;
+            else {
+                stream << std::forward<T>(value);
+                writef(stream, s + 1, std::forward<Args>(args)...); // call even when *s == 0 to detect extra arguments
+                return;
+            }
+        }
+        stream << *s++;
+    }
+    throw "extra arguments provided to printf";
+}
+
+
+/**
+write(std::cout, 1, ", ", 2, ", ", 3, std::endl); みたいに使う。
+末尾に改行つかない
+*/
+template <typename Stream, typename T>
+void write(Stream & stream, T && value)
+{
+    stream << std::forward<T>(value);
+}
+
+
+template <typename Stream, typename T, typename U, typename... Args>
+void write(Stream & stream, T&& value, U&& head, Args&&... args)
+{
+    stream << std::forward<T>(value);
+    write(stream, std::forward<U>(head), std::forward<Args>(args)...);
+}
+
+
+/**
+writefの末尾改行ありバージョン
+*/
+template <typename Stream, typename... Args>
+void writefln(Stream & stream, const char *s, Args&&... args)
+{
+    writef(stream, s, std::forward<Args>(args)...);
+    stream << std::endl;
+}
+
+
+/**
+writeの末尾改行ありバージョン
+*/
+template <typename Stream, typename T, typename... Args>
+void writeln(Stream & stream, T&& value, Args&&... args)
+{
+    write(stream, std::forward<T>(value), std::forward<Args>(args)...);
+    stream << std::endl;
+}
