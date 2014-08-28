2b074e1def05d2854ca9ec2ca985cc860ab77718
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-08-28 17:28:07 +0900

    enforceに使用方法を追加

--- include/exception.hpp
+++ include/exception.hpp
@@ -9,8 +9,17 @@
 
 namespace procon { namespace utils {
 
-/** value が false に評価可能であれば例外を投げます。
+/** value が false に評価可能であれば例外(`std::runtime_error`)を投げます。
 そうでない場合は、value をそのまま返します。
+
+Example:
+------------
+// 画像が存在しない場合には例外を投げる
+auto pb_opt = enforce(Problem::get("img1.ppm"), "cannot open image file.");
+auto& pb = *pb_opt;
+
+enforce(pb.div_x() > 0 && pb.div_y() > 0, "invalid problem.");
+------------
 */
 template <typename T>
 auto enforce(T&& value, std::string const & msg, const char* fname = __FILE__, std::size_t line = __LINE__)
