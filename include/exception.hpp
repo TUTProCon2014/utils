ddc8a8e958aafa230826804082d1201b060f1b01
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-08-28 17:29:00 +0900

    D言語のscope(exit)文相当のscopeExit関数の追加

--- include/exception.hpp
+++ include/exception.hpp
@@ -32,4 +32,48 @@ auto enforce(T&& value, std::string const & msg, const char* fname = __FILE__, s
 }
 
 
+template <typename F>
+struct ScopeExit
+{
+    ScopeExit(F& f) : _f(f) {}
+    ScopeExit(ScopeExit&& s) = default; // moveコピーコンストラクタは許可する
+    ~ScopeExit(){ _f(); }
+
+  private:
+    F& _f;
+
+    ScopeExit(ScopeExit const & s){}
+    void operator=(ScopeExit const & s) {}
+    void operator=(ScopeExit&& s) {}
+};
+
+
+/**
+D言語の`scope(exit)`文です。
+そのスコープを抜ける場合に関数を実行します。
+使用例としては、例外安全なリソース管理があります。
+
+Example:
+------------
+int main()
+{
+    auto obj = new Obj(ctorArgument);
+
+    // たとえ例外が投げられたとしても、確実に`delete`する
+    auto scope = scopeExit([&](){
+        delete obj;
+    });
+
+    maybe_throw_func();
+
+    return 0;
+}
+------------
+*/
+template <typename F>
+ScopeExit<F> scopeExit(F& f)
+{
+    return ScopeExit<F>(f);
+}
+
 }} // namespace procon::utils
